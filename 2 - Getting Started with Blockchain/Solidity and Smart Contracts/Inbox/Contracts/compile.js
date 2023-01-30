a7c7352dbf1f6b5b36587ebb9ab1a7e0c4031e8c
bruno Canto <bruno_scanto@hotmail.com>
2023-01-30 10:51:56 -0300

    Criação de contratos

--- 2 - Getting Started with Blockchain/Solidity and Smart Contracts/Inbox/Contracts/compile.js
+++
@@ -1,9 +0,0 @@
-const path = require('path');
-const fs = require('fs');
-const solc = require('solc');
-
-const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol')
-const sourse = fs.readdirSync(inboxPath, 'utf8');
-
-module.exports = solc.compile(sourse, 1).contracts[':Inbox'];
-
--- 2 - Getting Started with Blockchain/Solidity and Smart Contracts/Inbox/test/Inbox.test.js
+++ 2 - Getting Started with Blockchain/Solidity and Smart Contracts/Inbox/test/Inbox.test.js
@@ -1,16 +1,25 @@
-const assert = require('assert');
-const ganache = requere('ganache-cli');
-const Web3 = require('web3');
+const assert = require("assert");
+const ganache = require("ganache-cli");
+const Web3 = require("web3");
 const web3 = new Web3(ganache.provider());
+const { interface, bytecode } = require("../compile");
 
-beforeEach(() => {
-    // Get a list of all accounts
-    web3.eth.getAccounts().then((fetchedAccounts) => {
-      console.log(fetchedAccounts);
-    });
+let accounts;
+let inbox;
+
+beforeEach(async () => {
+  // Get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({
+      data: bytecode,
+      arguments: ["Hi there!"],
+    })
+    .send({ from: accounts[0], gas: "1000000" });
+});
+
+describe("Inbox", () => {
+  it("deploys a contract", () => {
+    console.log(inbox);
   });
-  
-  describe("Inbox", () => {
-    it("deploys a contract", () => {});
-  });
-  
+});
