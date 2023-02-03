514061db6e272d0840655d3a1cdbfb65d63e846e
bruno Canto <bruno_scanto@hotmail.com>
2023-02-03 14:37:33 -0300

    Curso Ethereum ADD arquivos

---
+++ 2 - Getting Started with Blockchain/Solidity and Smart Contracts/Contracts/Kickstart/Ethereum/deploy.js
@@ -0,0 +1,27 @@
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProvider(
+  'ski unfold rough into project head original hazard pond use decade genuine',
+  // remember to change this to your own phrase!
+  'wss://goerli.infura.io/ws/v3/4524df95c22a4a5ca3aa7515af6fc14a'
+  // remember to change this to your own endpoint!
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+  provider.engine.stop();
+};
+deploy();
