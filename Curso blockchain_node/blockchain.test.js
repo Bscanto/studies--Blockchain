5147b0bf0987ae7b6a09354308dbe25ece1a2a5e
Bruno <bruno_scanto@hotmail.com>
2024-06-12 18:39:17 -0300

    Finalizando validadores da Blockchain

--- Curso blockchain_node/blockchain.test.js
+++ Curso blockchain_node/blockchain.test.js
@@ -29,4 +29,23 @@ describe('Blockchain', () => {
     expect(bc.isValidChain(bc2.chain)).toBe(false);
   });
   
+  it('invalidates a corrupt chain', () => {
+    bc2.addBlock('200U$');
+    bc2.chain[1].data = '0U$';
+    expect(bc.isValidChain(bc2.chain)).toBe(false);
+  });
+//==========
+
+it('replaces the chain with a valid chain', () => {
+	bc2.addBlock('600U$');
+  bc.replaceChain(bc2.chain);
+  expect(bc.chain).toEqual(bc2.chain);
+});
+
+it('does not replace the chain with one of less than or equal length', () => {
+	bc.addBlock('200U$');
+  bc.replaceChain(bc2.chain);
+  expect(bc.chain).not.toEqual(bc2.chain);
+});
+
 });
