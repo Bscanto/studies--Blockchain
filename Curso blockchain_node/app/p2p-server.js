30f201377e0970929cadd1920e781b1d62400011
Bruno <bruno_scanto@hotmail.com>
2024-06-26 16:28:26 -0300

    P2P

--- Curso blockchain_node/app/p2p-server.js
+++ Curso blockchain_node/app/p2p-server.js
@@ -1,1 +1,5 @@
 const websocket = require('ws');
+const P2P_PORT = process.env.P2P_PORT || 5001;
+const peers = process.env.PEERS ?
+
+$ HTTP_PORT = 3002 P2P_PORT = 5003  PEERS = ws://localhost:5001, ws://localhost:5002 npm run dev
