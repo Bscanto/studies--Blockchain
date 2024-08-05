33adcb2c08d67e358c5d97df0c860853db7819fd
Bruno <bruno_scanto@hotmail.com>
2024-08-05 13:37:18 -0300

    sockets

--- Curso blockchain_node/app/p2p-server.js
+++ Curso blockchain_node/app/p2p-server.js
@@ -1,5 +1,39 @@
 const websocket = require('ws');
 const P2P_PORT = process.env.P2P_PORT || 5001;
-const peers = process.env.PEERS ?
+const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
 
-$ HTTP_PORT = 3002 P2P_PORT = 5003  PEERS = ws://localhost:5001, ws://localhost:5002 npm run dev
+class P2pServer{
+  contructor(blockchain){
+    this.blockchain = blockchain;
+    this.socket = [];
+  }
+
+
+listen(){
+  const server = new Webspcket.Server({port: P2P_PORT});
+  server.on('connection', socket => this.connectSocket(socket));
+
+  this.connectToPeers();
+
+  console.log(`Listening for peer-to-peer connections on: ${P2P_PORT}`);
+}
+
+connectToPeers(){
+  peers.forEach(peer => {
+    const socket = new websocket(peer);
+    socket.on('open', () => this.connectSocket(socket));
+  });
+
+  const socket = new websocket(peer);
+  socket.on('open', () => this.connectSocket(socket));
+
+  //ws://localhost:5001
+}
+
+connectSocket(socket){
+  this.socket.push(socket);
+  console.log('Socket connected');
+}
+}
+
+module.exports = P2pServer;
